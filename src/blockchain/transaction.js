96bd82b4b8d7a10d118a176fd548b16334da0506
Louis-Frédéric Fortier <louis-frederic@concilio.com>
2020-05-13 15:45:41 +0200

    [+](Transaction) Add sign transaction method

--- src/blockchain/transaction.js
+++ src/blockchain/transaction.js
@@ -1,8 +1,26 @@
+const sha256 = require('crypto-js/sha256');
+
 class Transaction {
     constructor(from, to, amount) {
         this.from = from;
         this.to = to;
         this.amount = amount;
+        this.timeStamp = new Date();
+    }
+
+    createHash() {
+        return sha256(this.from + this.to + this.amount + this.timeStamp).toString();
+    }
+
+    signTransaction(key) {
+
+        if(key.getPublic('hex') !== this.from)
+            throw new Error('Cannot sign transaction for other wallets');
+
+        const hash = this.createHash();
+        const signature = key.sign(hash, 'base64');
+
+        this.signature = signature.toDER('hex');
     }
 }
 
