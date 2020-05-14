2619e9a0c321e4c10d2cfa4df0e38f5a2e3994c8
Louis-Frédéric Fortier <louis-frederic@concilio.com>
2020-05-14 17:52:26 +0200

    [+](Example) usage example in index.js

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const Wallet = require('./src/wallet/wallet');
 const wallet = new Wallet();
 
 LoufiCoin.mineNewBlock(wallet.publicKey);
+
+console.log('\x1b[32m[WALLET]\x1b[0m\tBALANCE: ', wallet.getBalance());
+
+LoufiCoin.mineNewBlock(wallet.publicKey);
+
+console.log('\x1b[32m[WALLET]\x1b[0m\tBALANCE: ', wallet.getBalance());
+
+LoufiCoin.mineNewBlock(wallet.publicKey);
+
+console.log('\x1b[32m[WALLET]\x1b[0m\tBALANCE: ', wallet.getBalance());
+
+LoufiCoin.mineNewBlock(wallet.publicKey);
+
+console.log('\x1b[32m[WALLET]\x1b[0m\tBALANCE: ', wallet.getBalance());
+
 LoufiCoin.mineNewBlock(wallet.publicKey);
 
 console.log('\x1b[32m[WALLET]\x1b[0m\tBALANCE: ', wallet.getBalance());
